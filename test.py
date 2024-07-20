cd95fd4d1d06dac7dc5acb385a88112d0e454f3e
Ettien Yann <ettienyane@gmail.com>
2024-07-20 14:57:11 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+# test_nltk.py
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+print("NLTK importé avec succès !")
